f7e83a4b491c4b753f39c45d55ff8564a2359a3f
break <fengyindian@126.com>
2020-03-07 11:00:41 +0800

    add build

---
+++ build/precache-manifest.c1e07215a60ba150663ca4e0144c7e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd1191ee85fc0a92b98344d3d810569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4503b4b881e889995f6f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bea8163965c03a55bdeb",
+    "url": "/static/js/2.b194959e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.b194959e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4503b4b881e889995f6f",
+    "url": "/static/js/main.9657cbaa.chunk.js"
+  },
+  {
+    "revision": "22a3bb7918d1d6838bb9",
+    "url": "/static/js/runtime-main.f916674d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
